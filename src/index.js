99b0dc822b6f40769942e3f3d7050503101ac4a0
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-01-03 17:30:28 -0400

    chore: Verify CPF already exist

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ const app = express();
 
 app.use(express.json());
 
-const costurmers = [];
+const custormers = [];
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.params;
 
-  costurmers.push({
+  const custormerAlreadyExist = custormers.some((custormer) => custormer.cpf === cpf);
+
+  if(custormerAlreadyExist) return response.status(400).json({ error: "CPF already exist"});
+
+  custormers.push({
     cpf,
     name,
     id: uuidV4(),
